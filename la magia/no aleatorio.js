83f665cf74f0b497bd338103a54e66a89acef0dd
cebollatibia <105892985+cebollatibia@users.noreply.github.com>
2022-12-13 19:13:16 -0600

    first commit

---
+++ la magia/no aleatorio.js
@@ -0,0 +1,14 @@
+const yesbtn = document.querySelector('#botonsi')
+yesbtn.addEventListener('click', ()=>{
+    alert('sabia que si podias')
+})
+const nobtn = document.querySelector('#botonno')
+nobtn.addEventListener('mouseover', ()=>{
+    const randomX = Math.random()*90
+    const randomY = Math.random()*90
+    nobtn.style.position = 'absolute'
+    nobtn.style.top = randomY+'%'
+    nobtn.style.left = randomX+'%'
+    nobtn.style.transform = 'translate(-${randomY}%)'
+    nobtn.style.transform = 'translate(-${randomX}%)'
+})
